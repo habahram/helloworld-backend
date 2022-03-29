e83d56686d0599c8976b479b27bd2bc6c0804a3a
habahram <habahram@email.arizona.edu>
2022-03-29 11:20:40 -0700

    commnets added.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//dependencies
 const express = require('express');
 const { store } = require('./temporarily-store/store');
 
@@ -9,37 +10,37 @@ const port = 4002;
 application.use(express.json());
 
 
-
+//methods
 application.get('/', (request, response) => {
-    response.status(200).json({done: true, message: 'Fine!'});
+    response.status(200).json({ done: true, message: 'Fine!' });
 });
 
 application.post('/register', (request, response) => {
     let name = request.body.name;
     let email = request.body.email;
-    let password = request.body.password;   
+    let password = request.body.password;
     store.addCustomer(name, email, password);
-    response.status(200).json({done: true, message: 'The customer was added successfully!'});
+    response.status(200).json({ done: true, message: 'The customer was added successfully!' });
 });
 
-application.post('/login', (request, response) => {    
+application.post('/login', (request, response) => {
     let email = request.body.email;
-    let password = request.body.password;   
+    let password = request.body.password;
     let result = store.login(email, password);
-    if(result.valid) {
-        response.status(200).json({done: true, message: 'The customer logged in successfully!'});
+    if (result.valid) {
+        response.status(200).json({ done: true, message: 'The customer logged in successfully!' });
     } else {
-        response.status(401).json({done: false, message: result.message});
-    }   
+        response.status(401).json({ done: false, message: result.message });
+    }
 });
 
 application.get('/quiz/:id', (request, response) => {
     let id = request.params.id;
     let result = store.getQuiz(id);
-    if(result.done) {
-        response.status(200).json({done: true, result: result.quiz});
+    if (result.done) {
+        response.status(200).json({ done: true, result: result.quiz });
     } else {
-        response.status(404).json({done: false, message: result.message});
+        response.status(404).json({ done: false, message: result.message });
     }
 });
 
