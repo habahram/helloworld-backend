3ba8924cea6b861610d89424fce3c23ec27d0928
habahram <habahram@email.arizona.edu>
2022-04-06 15:44:34 -0700

    Lecture code April 5th posted.

---
+++ data_access/store.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+
+const connectionString =
+    `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
+
+const connection = {
+   connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
+   ssl: { rejectUnauthorized: false}
+}
+const pool = new Pool(connection);
+
+
+let store = {
+
+    addCustomer: (name, email, password) => {
+        const hash = bcrypt.hashSync(password, 10);
+        customers.push({id: 1, name: name, email: email, password: hash});
+    },
+
+    login: (email, password) => {
+        let customer = customers.find(x => x.email.toLowerCase() === email.toLowerCase());
+        if(customer) {
+            let valid = bcrypt.compareSync(password, customer.password);
+            if(valid) {
+                return {valid: true};
+            } else {
+                return {valid: false, message: 'Credentials are not valid.'}
+            }
+        } else {
+            return {valid: false, message: 'Email not found.'};
+        }
+    },
+
+    getQuiz: (id) => {
+       let quiz = quizzes.find(x => x.name.toLowerCase() === id.toLowerCase());
+       if(quiz) {
+           return {done: true, quiz};
+       } else {
+           return {done: false, message: 'No quiz with this name was found.'};
+       }
+    },
+
+    getScores: () => {
+        return scores;
+    }
+
+}
+
+module.exports = { store }
